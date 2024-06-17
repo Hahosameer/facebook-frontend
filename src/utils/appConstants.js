e5b39f7c06729536772baf6df88ce64f29ea9528
hahosameer <ks4063180@gmail.com>
2024-06-17 10:37:20 +0500

    serverUrl Change

--- src/utils/appConstants.js
+++ src/utils/appConstants.js
@@ -1,2 +1,2 @@
 //  export const serverUrl = "http://localhost:5000"
- export const serverUrl = "https://sameer-facebook-backend.vercel.app/";
+ export const serverUrl = "https://facebook-backend-tau.vercel.app";
