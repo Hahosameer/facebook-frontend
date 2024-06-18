dbaef0050e6d1f16d5ff57815e3d5d4e4c642ae6
hahosameer <ks4063180@gmail.com>
2024-06-18 21:15:42 +0500

    1  change reload page

--- src/utils/appConstants.js
+++ src/utils/appConstants.js
@@ -1,2 +1,2 @@
 //  export const serverUrl = "http://localhost:5000"
- export const serverUrl = "https://facebook-backend-psi.vercel.app";
+ export const serverUrl = "https://facebook-backend-tawny.vercel.app";
